2d638125dda3f7377a04c9efb95030f3a68d6dd1
Arian Khani <ariankhani8213@gmail.com>
2025-03-27 11:24:45 +0330

    fix: fix some file

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "sqlite:///./shop.db"  # Using SQLite for simplicity
 
