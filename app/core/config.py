957b1895293fa306a2988183addb2b10b49e9d11
Arian Khani <ariankhani8213@gmail.com>
2025-03-29 11:47:53 +0330

    feat: seperate docker prod and dev

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Univercity Scheduler"
-    database_url: str = "sqlite:///./test.db"
+    database_url: str = "sqlite:///./shop.db"
     jwt_secret: str = "aae#g$1y7mb-i#4xrtc^p5zs9!056382u**7nl9i*_o#by9&lv"  # noqa: S105
     jwt_algorithm: str = "HS256"
     jwt_expiration_minutes: int | None = None  # None means no expiration (infinite)
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     ALLOWED_CONTENT_TYPES: set[str] = {"image/jpeg", "image/png"}
     ALLOWED_EXTENSIONS: set[str] = {'.jpg', '.jpeg', '.png'}
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
 
 
 
