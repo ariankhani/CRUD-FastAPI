b45699cd81a89c603d656ae1f9a9cb27bda911fe
Arian Khani <ariankhani8213@gmail.com>
2025-04-08 16:17:04 +0330

    fix: fix file

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta, timezone
 
 import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
 from app.core.config import settings
@@ -37,3 +39,17 @@ def decode_access_token(token: str) -> dict | None:
         )
     except jwt.PyJWTError:
         return None
+
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    user_data = decode_access_token(token)
+    if user_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
+    return user_data
