a1f017999b84bde4eaad63f00379b0c85c0c057a
Arian <ariankhani8213@gmail.com>
2025-05-02 17:52:00 +0330

    fix: fix logging setting

--- app/core/logger.py
+++ app/core/logger.py
@@ -3,23 +3,22 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 
-def setup_logger(name, log_file, level=None):
-    # تنظیم سطح لاگ بر اساس محیط (development یا production)
+def setup_logger(name, log_file, level=None, log_dir='logs') -> logging.Logger:
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_file_path = os.path.join(log_dir, log_file)
     if level is None:
         level = logging.DEBUG if os.getenv("ENV") == "development" else logging.INFO
 
-    # فرمت لاگ
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # handler برای فایل با قابلیت چرخش (rotate)
-    file_handler = RotatingFileHandler(log_file, maxBytes=10000, backupCount=5)
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=10000, backupCount=5)
     file_handler.setFormatter(formatter)
 
-    # handler برای نمایش در کنسول
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
 
-    # ساخت logger
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(file_handler)
@@ -27,7 +26,6 @@ def setup_logger(name, log_file, level=None):
 
     return logger
 
-# تعریف loggerهای مختلف
 product_logger: logging.Logger = setup_logger('products', 'products.log')
 order_logger: logging.Logger = setup_logger('orders', 'orders.log')
 main_logger: logging.Logger = setup_logger('main', 'app.log')
